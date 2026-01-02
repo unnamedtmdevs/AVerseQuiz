351ad46f0c24486753af7830fa8fe1a20de96c09
ludik <bakhanetssimon@gmail.com>
2026-01-03 03:24:20 +0700

    Initial Commit

---
+++ AVerse Quiz/AVerse_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVerse_QuizApp.swift
+//  AVerse Quiz
+//
+//  Created by Simon Bakhanets on 03.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct AVerse_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AVerse QuizTests/AVerse_QuizTests.swift
@@ -0,0 +1,16 @@
+//
+//  AVerse_QuizTests.swift
+//  AVerse QuizTests
+//
+//  Created by Simon Bakhanets on 03.01.2026.
+//
+
+import Testing
+
+struct AVerse_QuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
